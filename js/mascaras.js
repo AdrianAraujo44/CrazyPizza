2b7ddca5c863e649589d1c42ec603973aa5d50ea
AdrianAraujo <adrianaraujo247@gmail.com>
2021-04-07 20:21:20 -0300

    permitindo apenas numeros nos campos telefone e cep

--- js/mascaras.js
+++ js/mascaras.js
@@ -1,8 +1,20 @@
-function formatarCEP(campo){
-    campo.value=campo.value.replace(/\D/g,"")
-    campo.value=campo.value.replace(/^(\d{5})(\d)/,"$1-$2")
-  }
+function apenasNumeros(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    //var regex = /^[0-9.,]+$/;
+    var regex = /^[0-9.]+$/;
+    if( !regex.test(key) ) {
+       theEvent.returnValue = false;
+       if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+ }
+
+function formatarCEP(campo) {
+    campo.value = campo.value.replace(/\D/g, "")
+    campo.value = campo.value.replace(/^(\d{5})(\d)/, "$1-$2")
+}
 
 function formatarTelefone(campo) {
-    campo.value = campo.value.replace(/(\d)(\d{4})$/,"$1-$2") 
+    campo.value = campo.value.replace(/(\d)(\d{4})$/, "$1-$2")
 }
